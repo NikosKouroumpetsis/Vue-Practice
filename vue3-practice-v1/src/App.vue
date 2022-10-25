84dfa55f698b8c3dcacb81eaff9d48235e5a3458
Nikolaos Kouroumpetsis <nikolaos.kouroumpetsis@gmail.com>
2022-10-25 10:30:54 +0300

    first commit

---
+++ vue3-practice-v1/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 v-if="isShowing">{{ title }}</h1>
+  <h3 v-else-if="isShowing === null">This is v-else-if</h3>
+  <h2 v-else>This is if else part</h2>
+  <p>{{ para }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "This is title inside h1 tag",
+      isShowing: null,
+      para: "a example with props",
+    };
+  },
+};
+</script>
+
+<style></style>
